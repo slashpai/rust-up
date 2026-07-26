bc2fd85ec82457a9fdc0484169ac7035972a6752
Jayapriya Pai <slashpai9@gmail.com>
2026-07-26 12:20:58 +0530

    feat: add string formatter, changing variables usage
    Signed-off-by: Jayapriya Pai <slashpai9@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 // const should always have type
 const DIVIDER: &str = "---------------------------------------------------";
 
+fn string_formatting_placeholders() {
+    println!("Hello, {}!", "world");
+    println!("{name}", name = "Alice");
+}
+
 fn variables() {
     // Examples: type of a variable is decided by the value you give it.
     let literal = 'C';
@@ -11,7 +16,6 @@ fn variables() {
     println!("You have set integer number as {}", num);
     let floatnum = 10.5;
     println!("You have set decimal number as {}", floatnum);
-
     // Examples: setting types explicitly
     let name_explicit: &str = "Alice";
     print!("Name is set as {} \n", name_explicit);
@@ -20,8 +24,19 @@ fn variables() {
     print!("Name is set as {} \n", name_explicit2);
 }
 
+fn change_variables() {
+    let mut x = 5;
+    println!("Before changing value: {}", x);
+    x = 10;
+    println!("After changing value: {}", x);
+}
+
 fn main() {
-    println!("Hello Rust!\n{}", DIVIDER);
-    println!("Using variables\n{}", DIVIDER);
+    println!("Hello Rust!");
+    println!("{}\nString Formatting Placeholders\n{}", DIVIDER, DIVIDER);
+    string_formatting_placeholders();
+    println!("{}\nUsing variables\n{}", DIVIDER, DIVIDER);
     variables();
+    println!("{}\nChanging variables\n{}", DIVIDER, DIVIDER);
+    change_variables();
 }
